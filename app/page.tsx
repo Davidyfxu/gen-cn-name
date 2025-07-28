29bfba9630ecd05ce36c2f5b1e9a881ea29bb446
davidyfxu <yifanxu2020@gmail.com>
2025-07-28 17:04:49 +0800

    feat: add channel.io web SDK integration

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,11 @@ import {
   ChevronUp,
 } from "lucide-react";
 import Link from "next/link";
+import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 
+ChannelService.boot({
+  "pluginKey": "768b3c09-4470-44e2-b97d-b679e126ba8d", // fill your plugin key
+});
 export default function Home() {
   const { user, loading } = useAuth();
   const [showAuthModal, setShowAuthModal] = useState(false);
