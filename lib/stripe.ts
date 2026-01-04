48935e2d1e88d8e18a776c403ebd98b80c0e6265
davidyfxu <s200048@e.ntu.edu.sg>
2026-01-04 23:03:29 +0800

    refactor: simplify Stripe initialization by removing unnecessary API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,8 +4,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("STRIPE_SECRET_KEY is not set in environment variables");
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-06-30.basil",
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!;
